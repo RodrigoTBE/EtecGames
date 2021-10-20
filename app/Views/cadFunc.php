9336f7a2de5719178540cd2aaf593b04c6e6ab5c
RodrigoTBE <rodrigo.tarcis2@gmail.com>
2021-10-20 18:46:53 -0300

    Apenas formatando o código

--- app/Views/cadFunc.php
+++ app/Views/cadFunc.php
@@ -8,7 +8,7 @@
         <button type="submit" class="btn btn-primary">Buscar</button>
     </div>
 
-<?php
+    <?php
 $request = service('request');
 $codUsuFunc = isset($usuario->codusu)?$usuario->codusu:0;
 $emailUsu = isset($usuario->emailUsu)?$usuario->emailUsu:'';
@@ -16,24 +16,24 @@ $emailUsu = isset($usuario->emailUsu)?$usuario->emailUsu:'';
 
     <h1 class="h1 text-center">Funcionário Encontrado</h1>
 
-<form method="POST">
-    
-    <div class="mb-3">
-        <label for="codUsuFunc" class="form-label">Código do seu usuário</label>
-        <input type="text" class="form-control" name="codUsu" id="codUsuFunc" value="<?=$codUsuFunc?>" readonly>
-    </div>
-    <div class="mb-3">
-        <label for="emailUsu" class="form-label">E-mail Usuário</label>
-        <input type="text" class="form-control" name="emailusu" id="emailusu" value="<?=$emailUsu?>" readonly>
-    </div>
-    <div class="mb-3">
-        <label for="nomeFunc" class="form-label">Nome</label>
-        <input type="text" class="form-control" name="nomeFunc" id="nomeFunc" aria-describedby="emailHelp">
-    </div>
-    <div class="mb-3">
-        <label for="foneFunc" class="form-label">Telefone</label>
-        <input type="text" class="form-control" name="foneFunc" id="foneFunc">
-    </div>
-    <button type="submit" class="btn btn-primary">Cadastrar</button>
-</form>
+    <form method="POST">
+
+        <div class="mb-3">
+            <label for="codUsuFunc" class="form-label">Código do seu usuário</label>
+            <input type="text" class="form-control" name="codUsu" id="codUsuFunc" value="<?=$codUsuFunc?>" readonly>
+        </div>
+        <div class="mb-3">
+            <label for="emailUsu" class="form-label">E-mail Usuário</label>
+            <input type="text" class="form-control" name="emailusu" id="emailusu" value="<?=$emailUsu?>" readonly>
+        </div>
+        <div class="mb-3">
+            <label for="nomeFunc" class="form-label">Nome</label>
+            <input type="text" class="form-control" name="nomeFunc" id="nomeFunc" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="foneFunc" class="form-label">Telefone</label>
+            <input type="text" class="form-control" name="foneFunc" id="foneFunc">
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
 </form>
